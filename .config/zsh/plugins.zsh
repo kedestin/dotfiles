f05efe1c35d103a82ee7814b8dd6aa7a9cf274a9
kedestin <26238343+kedestin@users.noreply.github.com>
2021-11-03 21:02:18 -0400

    Split .zshrc and replace antibody with zcomet

---
+++ .config/zsh/plugins.zsh
@@ -0,0 +1,46 @@
+if [[ ! -f ${ZDOTDIR:-${HOME}}/.zcomet/bin/zcomet.zsh ]]; then
+  command git clone https://github.com/agkozak/zcomet.git ${ZDOTDIR:-${HOME}}/.zcomet/bin
+fi
+
+source ${ZDOTDIR:-${HOME}}/.zcomet/bin/zcomet.zsh
+
+# Setup theme
+zcomet load 'mafredri/zsh-async'
+zcomet load 'kedestin/minimalxx'
+
+# zsh niceties
+zcomet load 'hlissner/zsh-autopair'
+zcomet load 'kedestin/auto-ls'
+zcomet load 'agkozak/zsh-z'
+zcomet load 'popstas/zsh-command-time'
+zcomet load 'Tarrasch/zsh-autoenv' # cd gets really slow if allowed to look up
+zcomet load 'Tarrasch/zsh-functional'
+zcomet load 'zsh-users/zsh-autosuggestions'
+zcomet load 'zsh-users/zsh-completions'
+zcomet load 'zsh-users/zsh-syntax-highlighting'
+
+
+# Plugin Config
+
+AUTOENV_LOOK_UPWARDS=0
+
+ZSH_HIGHLIGHT_HIGHLIGHTERS=(main)
+ZSH_HIGHLIGHT_DIRS_BLACKLIST+=(/)
+ZSH_HIGHLIGHT_STYLES[path]='fg=cyan'
+
+ZSH_AUTOSUGGEST_STRATEGY=(completion)
+export ZSH_AUTOSUGGEST_USE_ASYNC=1
+
+MNML_PROMPT=(mnml_pyenv mnml_status)
+# export MNML_RPROMPT=(mnml_ssh 'mnml_cwd 2 0' mnml_git)
+MNML_RPROMPT=('mnml_cwd 2 0' mnml_git)
+export MNML_USER_CHAR=❯❯❯
+
+
+ZSH_COMMAND_TIME_MSG="elapsed %s"
+ZSH_COMMAND_TIME_COLOR="blue"
+
+export AUTO_LS_NEWLINE=false
+AUTO_LS_COMMANDS=(ls)
+
+ZSHZ_DATA=${XDG_DATA_HOME}/.z
