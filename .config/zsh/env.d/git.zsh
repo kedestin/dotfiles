48659476d11e0f5d433f6395796ee9ee4a9bcd4a
kedestin <26238343+kedestin@users.noreply.github.com>
2023-02-16 00:10:19 -0500

    zsh: Add dynamic named directory for current git repo root

--- .config/zsh/env.d/git.zsh
+++ .config/zsh/env.d/git.zsh
@@ -5,3 +5,9 @@ path+="$XDG_DATA_HOME/git/personal/bin"
 # Add completions for git scripts to fpath and autoload
 fpath+="$XDG_DATA_HOME/git/personal/zsh/completions"
 autoload -Uz "${fpath[-1]}"/*(.:t)
+
+
+fpath+="$XDG_DATA_HOME/git/personal/zsh/functions"
+autoload -Uz add-zsh-hook
+# Add implementation for dynamic named directory for root of current git repo
+add-zsh-hook -Uz zsh_directory_name _git_root_named_directory
