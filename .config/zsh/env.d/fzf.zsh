8e77d9917dc0148e85e9fb895a29f7f7adf34742
kedestin <26238343+kedestin@users.noreply.github.com>
2023-11-03 16:30:56 -0400

    feat(fzf): Try to resolve fzf share directory with fzf-share

--- .config/zsh/env.d/fzf.zsh
+++ .config/zsh/env.d/fzf.zsh
@@ -1,21 +1,26 @@
 #!/usr/bin/env zsh
-{
-  local distro=${(Q):-$(awk -F= '/^NAME/{print $2}' /etc/os-release)}
-  case "$distro"; in
-    Ubuntu)
-      source /usr/share/doc/fzf/examples/completion.zsh
-      source /usr/share/doc/fzf/examples/key-bindings.zsh
-    ;;
-    'Fedora Linux')
-      source /usr/share/zsh/site-functions/fzf
-      source /usr/share/fzf/shell/key-bindings.zsh
-    ;;
-    *)
-      echo "Do not know where to find fzf shell integrations"
-      ;;
-  esac
-}
 
+if command -v fzf-share > /dev/null ; then
+  source "$(fzf-share)"/completion.zsh
+  source "$(fzf-share)"/key-bindings.zsh
+else
+  {
+    local distro=${(Q):-$(awk -F= '/^NAME/{print $2}' /etc/os-release)}
+    case "$distro"; in
+      Ubuntu)
+        source /usr/share/doc/fzf/examples/completion.zsh
+        source /usr/share/doc/fzf/examples/key-bindings.zsh
+      ;;
+      'Fedora Linux')
+        source /usr/share/zsh/site-functions/fzf
+        source /usr/share/fzf/shell/key-bindings.zsh
+      ;;
+      *)
+        echo "Do not know where to find fzf shell integrations"
+        ;;
+    esac
+  }
+fi
 FZF_DEFAULT_COMMAND='fd -t file'
 
 
