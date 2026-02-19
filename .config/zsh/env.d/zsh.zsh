82cbbfe8785c40155c020cc7285c297b86dc6112
kedestin <26238343+kedestin@users.noreply.github.com>
2026-02-18 20:34:51 -0500

    feat(zsh): Ensure that run-help and some helpers are loaded
        run-help is bound to Alt+h by default, and allows you
        to bring up the manpage for commands in your ZLE buffer

--- .config/zsh/env.d/zsh.zsh
+++ .config/zsh/env.d/zsh.zsh
@@ -5,3 +5,7 @@ HISTFILE=${XDG_STATE_HOME}/zsh/history
 # Autoload personal zsh functions
 fpath+="$XDG_DATA_HOME/zsh/personal/functions"
 autoload -Uz "${fpath[-1]}"/*(.:t)
+
+# Load run-help (Alt+h in emacs keybinds) and helpers
+autoload -Uz run-help
+autoload -Uz run-help-git run-help-ip run-help-openssl run-help-sudo
